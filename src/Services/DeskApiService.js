c3664efe701a437d350e5be04ddfae3c5f3905bf
Justin Luong <cong.itsoft@gmail.com>
2020-05-22 15:46:15 +0700

    fix bug

--- src/Services/DeskApiService.js
+++ src/Services/DeskApiService.js
@@ -2,7 +2,7 @@ import {http} from "./ApiService";
 
 const DeskApiService = {
     getById: (id) => http.get(`/api/v1/desks/${id}`),
-    list: (params = {}) => http.get('/api/v1/me/desks', {...params}),
+    list: (params = {}) => http.get('/api/v1/me/desks', {params}),
     create: (data) => http.post('/api/v1/desks', {...data}),
     update: (id, data) => http.patch(`/api/v1/desks/${id}`, data),
     delete: (id) => http.delete(`/api/v1/desks/${id}`),
